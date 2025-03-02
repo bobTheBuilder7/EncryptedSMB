2115b50581dc4d41a4072a39599116d14ac939be
Suren Adzhamoglyan <adgsuren@gmail.com>
2025-03-02 00:44:17 -0700

    first commit

---
+++ EncryptedSMB.go
@@ -0,0 +1,118 @@
+package EncryptedSMB
+
+import (
+	"context"
+	"io"
+	"net"
+
+	"filippo.io/age"
+	"github.com/hirochachacha/go-smb2"
+)
+
+type EncryptedSMB struct {
+	share     *smb2.Share
+	recipient age.Recipient
+	identity  *age.ScryptIdentity
+	session   *smb2.Session
+}
+
+func New(ctx context.Context, address, username, password, shareName, encryptionPassword string) (*EncryptedSMB, error) {
+	var d net.Dialer
+	conn, err := d.DialContext(ctx, "tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	smbDialer := &smb2.Dialer{
+		Initiator: &smb2.NTLMInitiator{
+			User:     username,
+			Password: password,
+		},
+	}
+
+	s, err := smbDialer.DialContext(ctx, conn)
+	if err != nil {
+		return nil, err
+	}
+
+	share, err := s.Mount(shareName)
+	if err != nil {
+		return nil, err
+	}
+
+	recipient, err := age.NewScryptRecipient(encryptionPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	identity, err := age.NewScryptIdentity(encryptionPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	return &EncryptedSMB{
+		share:     share,
+		recipient: recipient,
+		identity:  identity,
+		session:   s,
+	}, nil
+}
+
+func (e *EncryptedSMB) Logoff() {
+	e.session.Logoff()
+}
+
+func (e *EncryptedSMB) Encrypt(path string, src io.Reader) error {
+	newFile, err := e.share.Create(path)
+	if err != nil {
+		return err
+	}
+
+	w, err := age.Encrypt(newFile, e.recipient)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(w, src)
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	err = newFile.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type EReadClose struct {
+	io.Reader
+	file *smb2.File
+}
+
+func (e *EReadClose) Close() error {
+	return e.file.Close()
+}
+
+func (e *EncryptedSMB) Decrypt(path string) (io.ReadCloser, error) {
+	encryptedFile, err := e.share.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := age.Decrypt(encryptedFile, e.identity)
+	if err != nil {
+		return nil, err
+	}
+
+	return &EReadClose{
+		file:   encryptedFile,
+		Reader: reader,
+	}, nil
+}
